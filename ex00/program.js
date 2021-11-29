3ed187594205f6b45ff0451aea925004a83d8356
KlodIt <c.buisson@it-students.fr>
2021-11-29 18:14:57 +0100

    fix ex00

--- ex00/program.js
+++ ex00/program.js
@@ -1,29 +1,26 @@
-let array = [ 3, 5, -4, 8, 11, 1, -1, 6];
-let targetSum = 10;
-
 function twoNumberSum(array, targetSum){
     // écrivez votre code ici.
     let array_result = [];
     let i = 0;
     // iter sur un tableau de données et incrémente l'index i 
     array.forEach(num => {
-        let j = 0;
         i++;
-            // iter sur un tableau de données et incrémente l'index j. 
-            // vérifie que les valeurs numérique ne s'ajoute pas avec elle même
-            // retourn un tableau de tableau de valeur dont le résultat est égale à targetSum 
+        // iter sur un tableau de données et incrémente l'index j. 
+        // vérifie que les valeurs numérique ne s'ajoute pas avec elle même
+        // retourn un tableau de tableau de valeur dont le résultat est égale à targetSum 
+        let j = 0;
         array.forEach(el => {
             j++;
-            if(i != j){
+            if(i > j+1){
                 result = num + el;
                 if (result == targetSum){
-                     array_result.push([num, el]);
+                    array_result.push([num, el]);
                 } 
             }
         });
     });
  return array_result;
 }
-console.log(twoNumberSum(array, targetSum));
+twoNumberSum(array, targetSum);
     // Merci de ne pas effacer la ligne en dessous.
     exports.twoNumberSum =  twoNumberSum;
