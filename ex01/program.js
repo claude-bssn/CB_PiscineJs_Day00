7b901f38d0fff9ba860750615f60458e7b96f5ee
KlodIt <c.buisson@it-students.fr>
2021-11-29 12:19:02 +0100

    ex01

---
+++ ex01/program.js
@@ -0,0 +1,13 @@
+let string = "kayak";
+
+function isPalindrome(string){
+    let palindrome = string.split("").reverse().join("")
+    if (string === palindrome){
+        return true;
+    }else{
+        return false;
+    }
+}
+    isPalindrome(string);
+    // Merci de ne pas effacer la ligne en dessous.
+    exports.isPalindrome =  isPalindrome;
